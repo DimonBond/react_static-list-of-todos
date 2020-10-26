3096ed36ef7fb6c5c5c9c09ec25b34080d159cba
dmitriy <dmitriy.bondarenko.it@gmail.com>
2020-10-26 12:13:04 +0200

    Updates

---
+++ precache-manifest.f288db71d84b42aadac72e89e31196ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c92f7b3ebc936c1b64ed7fe1ceae7d56",
+    "url": "./index.html"
+  },
+  {
+    "revision": "14606b5d4aa44d0dff74",
+    "url": "./static/css/main.6ad4c35e.chunk.css"
+  },
+  {
+    "revision": "edcc8acd3c83803ea5af",
+    "url": "./static/js/2.18aeaae4.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.18aeaae4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14606b5d4aa44d0dff74",
+    "url": "./static/js/main.8801d29c.chunk.js"
+  },
+  {
+    "revision": "ec438ca3dae444460a4d",
+    "url": "./static/js/runtime-main.1d48480d.js"
+  }
+]);
